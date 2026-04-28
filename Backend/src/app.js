07df6ed98a8d985c460763d0c7eb8413002502f5
Jayesh Mandawara <150209576+Jayesh-Mandawara@users.noreply.github.com>
2026-04-28 22:06:33 +0530

    Updated auth routes in app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-// Routes
+// Auth Routes
 const authRoutes = require("./routes/auth.routes");
 app.use("/api/auth", authRoutes);
 
