bff8adb3beecd407ffc7c11a10d8807106c6b74f
Jayesh Mandawara <150209576+Jayesh-Mandawara@users.noreply.github.com>
2026-04-30 21:58:38 +0530

    Update blacklist.model.js with some Info on token blacklisting
    Added comments regarding token blacklisting methods.

--- Backend/src/models/blacklist.model.js
+++ Backend/src/models/blacklist.model.js
@@ -1,3 +1,5 @@
+// This is not the appropriate way to blacklist the token but instead of this we can use Reddis 
+// which is very fast as compared to MongoDB.
 const mongoose = require("mongoose");
 
 const blackListSchema = new mongoose.Schema(
